eef3f571947d1aa31156c59820057d726f1d9dab
tgc624 <fummy.tgc@gmail.com>
2019-09-22 20:22:22 +0900

    For disable vue/max-attributes-per-line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,30 @@
 module.exports = {
   root: true,
+  plugins: ["vue"],
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "@vue/typescript",
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+    // "prettier/prettier": [{}]
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
