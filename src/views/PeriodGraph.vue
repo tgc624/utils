bad9276535a2d0365bfa283c07892a9db700ef38
tgc624 <fummy.tgc@gmail.com>
2019-10-05 07:08:19 +0900

    init

---
+++ src/views/PeriodGraph.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-content></v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PriodGraph",
+  components: {},
+  data: (): {} => ({}),
+  computed: {},
+  methods: {}
+});
+</script>
