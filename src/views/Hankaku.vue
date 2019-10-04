04fff4717fcfffd3134dadbc1c310f97234b9ae9
tgc624 <fummy.tgc@gmail.com>
2019-10-05 06:49:08 +0900

    関数を使えない環境でコピーしやすいようにした

--- src/views/Hankaku.vue
+++ src/views/Hankaku.vue
@@ -14,7 +14,7 @@
               rounded
             />
           </v-col>
-          <v-col cols="12">
+          <v-col v-if="isCopyable" cols="12">
             <v-text-field
               id="hankaku"
               append-outer-icon="file_copy"
@@ -25,6 +25,11 @@
               :value="hankaku"
               @click:append-outer="copy"
             />
+          </v-col>
+          <v-col v-else cols="12">
+            <v-card>
+              <v-card-text>{{ hankaku }}</v-card-text>
+            </v-card>
           </v-col>
           <v-col>
             <span>全角カタカナを半角カタカナにします。</span>
@@ -53,8 +58,15 @@ export default Vue.extend({
     showSnackbar: false
   }),
   computed: {
-    hankaku: function() {
+    hankaku: function(): string {
       return this.zenkaku ? zenkaku2hankaku(this.zenkaku) : "";
+    },
+    isCopyable: (): boolean => {
+      return !!(
+        navigator &&
+        navigator.clipboard &&
+        navigator.clipboard.writeText
+      );
     }
   },
   methods: {
